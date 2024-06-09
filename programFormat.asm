1b4986d28210881117a8d9385c3dfa678f6b4b7d
Robert <robertpearcetv@gmail.com>
2024-06-09 13:42:38 -0700

    Added programFormat file

---
+++ programFormat.asm
@@ -0,0 +1,32 @@
+;--------------------------------
+; Variable Declarations
+section .data
+
+; The data section is where initialized data must be
+; declared. Variables and constants will be in this
+; Section. Data defined here is static and remains
+; throughout the program execution.
+
+; Example of defining a string.
+hello   db  'Hello, world!'
+
+; Example of defining an integer.
+num     db  10
+
+;--------------------------------
+; Code Section
+section .text
+
+global _start
+_start:
+
+; The code is places under the text section.
+; This section contains the executable instructions
+; of the program. There are headers and lebels used
+; to define the initial entry point of the program
+; and various other code segments.
+
+
+;--------------------------------
+; Terminate Program
+last:
