11c5ed751c3b656c10054b63c4b377a56ed74a8d
Robert <robertpearcetv@gmail.com>
2024-06-09 14:04:46 -0700

    basicArithmetic update

--- basicArithmetic.asm
+++ basicArithmetic.asm
@@ -1,17 +1,36 @@
 ;--------------------------------
-; Variable Declarations
+; Initialized Data Section
 section .data
 
+num1    db  10  ; Initialize byte for 1st int.
+num2    db  5   ; Initialize byte for 2nd int.
+
+;--------------------------------
+; Uninitialized Data Section
 section .bss
+
+result resb 1   ; Reserve byte for result.
 
 ;--------------------------------
 ; Code Section
 section .text
 
-global _start
+global _start   ; Entry point for the program.
 _start:
 
+; Addition
 
+
+
+; Subtraction
+
+
+
+; Multiplication
+
+
+
+; Division
 
 ;--------------------------------
 ; Terminate Program
