7e92e8e2b8c6237b3998945fd371304431366bca
Robert Pearce <102342225+RobertxPearce@users.noreply.github.com>
2024-06-09 17:53:29 -0700

    Update 03controlStructures.asm

--- 03controlStructures.asm
+++ 03controlStructures.asm
@@ -1,1 +1,36 @@
+; Assemble the file with:
+; yasm -f elf64 -g dwarf2 -o 03controlStructures.o 03controlStructures.asm
 
+; Link the program with:
+; ld -m elf_x86_64 -o 03controlStructures 03controlStructures.o
+
+; Debug with:
+; ddd 03controlStructures
+
+;--------------------------------
+; Initialized Data Section
+section .data
+
+EXIT_SUCCESS    equ 0   ; Successful Operation
+SYS_exit        equ 60  ; Call Code for Terminate
+
+
+;--------------------------------
+; Uninitialized Data Section
+section .bss
+
+
+;--------------------------------
+; Code Section
+section .text
+
+global _start   ; Entry point for the program.
+_start:
+
+
+;--------------------------------
+; Terminate Program
+last:
+    mov rax, SYS_exit       ; Call code for exit.
+    mov rdi, EXIT_SUCCESS   ; Exit program with success.
+    syscall
