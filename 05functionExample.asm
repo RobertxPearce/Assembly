cc455d5f289853ea67a9175d6439937944cd1db8
robertxpearce <robertbdpearce@gmail.com>
2024-06-16 09:35:14 -0700

    Added function example file.

---
+++ 05functionExample.asm
@@ -0,0 +1,34 @@
+;-----------------------
+; Robert Pearce
+; 06/17/2024
+
+;-------------------------------------------------------
+; BSS (Block Start Symbol) Uninitialized Data Section
+section .bss
+
+
+
+;-------------------------------------------------------
+; Data Initialized Variable Declarations
+section .data
+
+
+
+EXIT_SUCCESS    equ 0   ; Exit code for success.
+SYS_exit        equ 60  ; Syscall code for terminating program.
+
+;-------------------------------------------------------
+; Code Section
+section .text
+
+global _start
+_start:
+
+
+
+;-------------------------------------------------------
+; Terminate Program
+last:
+    mov rax, SYS_exit       ; Load the syscall code for terminating the program.
+    mov rdi, EXIT_SUCCESS   ; Load the exit value for the program into rdi.
+    syscall
